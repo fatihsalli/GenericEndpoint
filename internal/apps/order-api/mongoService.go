a6a0b82e883156cde930e23b34fbdfc1f52b13a8
fatihsalli <mimsallifatih@gmail.com>
2023-05-06 23:20:09 +0300

    elasticsearch-mongo

--- internal/apps/order-api/mongoService.go
+++ internal/apps/order-api/mongoService.go
@@ -108,3 +108,5 @@ func (s *MongoService) FromModelConvertToFilter(req OrderGetRequest) (bson.M, *o
 
 // TODO: MongoDB ile response dönerken interface döndüğümde key-value olarak yazıyor model döndüğümde ise gereksiz olarak tüm fieldları dönüyor.
 // TODO: ID değeri elasticsearch tarafında girilmeyince geri dönmüyor bence dönülmeli
+
+// TODO: Elasticsearch match
